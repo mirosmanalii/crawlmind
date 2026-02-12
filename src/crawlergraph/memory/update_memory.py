7d282b43f8fc89fe9017be5fda8358a55c3c6021
Mir Osman Ali <w.mirosmanali@gmail.com>
2026-02-12 18:02:53 +0530

    Removed DOM argument

--- src/crawlergraph/memory/update_memory.py
+++ src/crawlergraph/memory/update_memory.py
@@ -1,21 +1,9 @@
 from crawlergraph.state import CrawlState
 from crawlergraph.memory.loop_guards import compute_page_hash
 
-
-def update_memory(state: CrawlState, dom: str) -> CrawlState:
-    """
-    Updates crawl memory after a successful page observation.
-    """
-
-    page_hash = compute_page_hash(dom)
-
-    # Store fingerprint in state
-    state.page_hash = page_hash
-
-    # Track visited URLs
+def update_memory(state: CrawlState) -> CrawlState:
     state.visited_pages.add(state.current_url)
 
-    # Track URL visit counts
     state.url_visit_counts[state.current_url] = (
         state.url_visit_counts.get(state.current_url, 0) + 1
     )
